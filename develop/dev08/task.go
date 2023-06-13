2df4d391a72a96d1e08357e9207bd2634b2fd7ae
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-06-13 16:52:42 +0300

    Added the 8th task.

---
+++ develop/dev08/task.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	for {
+		// Приглашение
+		fmt.Print("P$ ")
+
+		// Ридер для чтения команд
+		reader := bufio.NewReader(os.Stdin)
+
+		// Получаем введенную команду
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		args := strings.Split(input, " ")
+
+		if len(args) == 0 {
+			continue
+		}
+
+		switch cmdName := args[0]; cmdName {
+		// Блок выхода
+		case "exit":
+			return
+		// Блок cd
+		case "cd":
+			// Если аргументы отсутствуют, то меняем
+			// текущую директорию на пользовательскую
+			if len(args) < 2 {
+				wordDirectory, err := os.UserHomeDir()
+				if err != nil {
+					fmt.Println(err)
+				}
+				args = append(args, wordDirectory)
+			}
+			// В обратном случае, просто меняем директорию
+			// на указанную в аргументе
+			err := os.Chdir(args[1])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+		// Данный блок покрывает большинство утилит:
+		// pwd, echo, kill, ps
+		default:
+			cmd := exec.Command(cmdName, args[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		}
+	}
+}
