42ed8b505bf77d4b3ab2aa3f6d9e4ccd5718cb9f
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-06-14 12:31:30 +0300

    Added the 9th task.

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,4 +1,58 @@
 package main
 
-// Реализовать утилиту wget с возможностью скачивать сайты
-// целиком.
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+)
+
+// Реализовать аналог утилиты wget
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <url>")
+		return
+	}
+
+	url := os.Args[1]
+
+	if err := urlValid(url); err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	base := filepath.Base(url)
+	file, err := os.Create(base)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	fmt.Println("Downloaded:", base)
+}
+
+func urlValid(rawURL string) error {
+	_, err := url.Parse(rawURL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
